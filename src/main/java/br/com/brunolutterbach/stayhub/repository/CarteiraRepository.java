7edbfdef5f795d5faa77b6ca793d2cad2f4e4b9b
Bruno <brunolutterbach13@gmail.com>
2023-11-06 20:10:22 -0300

    Adicionado anotação @Repository

--- src/main/java/br/com/brunolutterbach/stayhub/repository/CarteiraRepository.java
+++ src/main/java/br/com/brunolutterbach/stayhub/repository/CarteiraRepository.java
@@ -2,6 +2,8 @@ package br.com.brunolutterbach.stayhub.repository;
 
 import br.com.brunolutterbach.stayhub.model.carteira.Carteira;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CarteiraRepository extends JpaRepository<Carteira, Long> {
 }
--- src/main/java/br/com/brunolutterbach/stayhub/repository/UsuarioRepository.java
+++ src/main/java/br/com/brunolutterbach/stayhub/repository/UsuarioRepository.java
@@ -2,6 +2,8 @@ package br.com.brunolutterbach.stayhub.repository;
 
 import br.com.brunolutterbach.stayhub.model.user.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 }
