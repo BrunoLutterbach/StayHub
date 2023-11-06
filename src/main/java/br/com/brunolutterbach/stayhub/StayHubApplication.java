6804e400260e1b9bb72f8098205b86136d13f836
Bruno <brunolutterbach13@gmail.com>
2023-11-06 19:40:42 -0300

    Firs commit

---
+++ src/main/java/br/com/brunolutterbach/stayhub/StayHubApplication.java
@@ -0,0 +1,13 @@
+package br.com.brunolutterbach.stayhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StayHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StayHubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/brunolutterbach/stayhub/StayHubApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.brunolutterbach.stayhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StayHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
