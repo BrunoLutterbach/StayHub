479bc7db8fd196f51e677d6173ebbb23525572bc
Bruno <brunolutterbach13@gmail.com>
2023-11-07 19:40:28 -0300

    método criarUsuario criado

--- src/main/java/br/com/brunolutterbach/stayhub/service/UsuarioService.java
+++ src/main/java/br/com/brunolutterbach/stayhub/service/UsuarioService.java
@@ -1,5 +1,8 @@
 package br.com.brunolutterbach.stayhub.service;
 
+import br.com.brunolutterbach.stayhub.model.user.DadosCadastroUsuario;
+import br.com.brunolutterbach.stayhub.model.user.DadosListagemUsuario;
+import br.com.brunolutterbach.stayhub.model.user.Usuario;
 import br.com.brunolutterbach.stayhub.repository.UsuarioRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -10,4 +13,9 @@ public class UsuarioService {
 
     final UsuarioRepository repository;
 
+    public DadosListagemUsuario criarUsuario(DadosCadastroUsuario dados) {
+        var usuario = new Usuario(dados);
+        repository.save(usuario);
+        return new DadosListagemUsuario(usuario);
+    }
 }
