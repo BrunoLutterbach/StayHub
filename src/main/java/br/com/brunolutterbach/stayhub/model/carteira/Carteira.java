9600dc94bfed192357f14daf0f112c36e6040c29
Bruno <brunolutterbach13@gmail.com>
2023-11-07 19:36:18 -0300

    Adicionado notações Lombok

--- src/main/java/br/com/brunolutterbach/stayhub/model/carteira/Carteira.java
+++ src/main/java/br/com/brunolutterbach/stayhub/model/carteira/Carteira.java
@@ -1,17 +1,41 @@
 package br.com.brunolutterbach.stayhub.model.carteira;
 
+import lombok.*;
+import org.hibernate.proxy.HibernateProxy;
+
 import javax.persistence.*;
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.Objects;
 
 @Entity(name = "carteiras")
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
 public class Carteira {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private BigDecimal saldo;
+    private BigDecimal saldo = BigDecimal.ZERO;
     @OneToMany(mappedBy = "carteira")
+    @ToString.Exclude
     private List<Transacoes> transacoes;
 
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        Carteira carteira = (Carteira) o;
+        return getId() != null && Objects.equals(getId(), carteira.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+    }
 }
