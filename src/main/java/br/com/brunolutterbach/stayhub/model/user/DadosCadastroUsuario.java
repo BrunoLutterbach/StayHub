0b736b4a698308b4fa47ff96f4c00a9c79a29461
Bruno <brunolutterbach13@gmail.com>
2023-11-07 20:05:27 -0300

    Adicionado validação aos atributos

--- src/main/java/br/com/brunolutterbach/stayhub/model/user/DadosCadastroUsuario.java
+++ src/main/java/br/com/brunolutterbach/stayhub/model/user/DadosCadastroUsuario.java
@@ -1,9 +1,15 @@
 package br.com.brunolutterbach.stayhub.model.user;
 
+import javax.validation.constraints.NotBlank;
+
 public record DadosCadastroUsuario(
+        @NotBlank
         String nome,
+        @NotBlank
         String senha,
+        @NotBlank
         String email,
+        @NotBlank
         TipoUsuario tipoUsuario
 
 ) {
