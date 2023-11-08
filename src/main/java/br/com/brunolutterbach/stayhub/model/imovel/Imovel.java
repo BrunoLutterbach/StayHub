4a5b30effa6e251d2c0a79b9353398c20c2200b0
Bruno <brunolutterbach13@gmail.com>
2023-11-08 20:44:46 -0300

    Criação da classe Imovel

---
+++ src/main/java/br/com/brunolutterbach/stayhub/model/imovel/Imovel.java
@@ -0,0 +1,32 @@
+package br.com.brunolutterbach.stayhub.model.imovel;
+
+import br.com.brunolutterbach.stayhub.model.user.Usuario;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+@Entity(name = "imoveis")
+public class Imovel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String endereco;
+    private String cidade;
+    private String estado;
+    private String cep;
+    private CategoriaImovel categoria;
+    private String descricao;
+    @ElementCollection
+    private List<String> fotos;
+    private BigDecimal valor = BigDecimal.ZERO;
+    private Boolean disponibilidade;
+    private LocalDate datasDisponiveis;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "proprietario_id")
+    private Usuario proprietario;
+
+
+}
