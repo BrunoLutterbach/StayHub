5dd6ec1d88209922946ed3654a71a4fb082039ab
Bruno <brunolutterbach13@gmail.com>
2023-11-13 20:35:29 -0300

    endpoint para cadastrar imveol

--- src/main/java/br/com/brunolutterbach/stayhub/controller/ImovelController.java
+++ src/main/java/br/com/brunolutterbach/stayhub/controller/ImovelController.java
@@ -1,9 +1,17 @@
 package br.com.brunolutterbach.stayhub.controller;
 
+import br.com.brunolutterbach.stayhub.model.imovel.DadosCadastroImovel;
+import br.com.brunolutterbach.stayhub.model.imovel.DadosListagemImovel;
 import br.com.brunolutterbach.stayhub.service.ImovelService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/imovel")
@@ -11,6 +19,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class ImovelController {
 
     final ImovelService service;
+
+    @PostMapping("/cadastrar")
+    public ResponseEntity<DadosListagemImovel> cadastrarImovel(@RequestBody @Valid DadosCadastroImovel dados, UriComponentsBuilder uriBuilder) {
+        var imovel = service.cadastrarImovel(dados);
+        var uri = uriBuilder.path("/api/imovel/{id}").buildAndExpand(imovel.id()).toUri();
+        return ResponseEntity.created(uri).body(imovel);
+    }
 
 
 
