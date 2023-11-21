eb9a3fe89d3c06f3f5caaf4b648b5e14d386d94d
Bruno <brunolutterbach13@gmail.com>
2023-11-21 20:42:44 -0300

    Criado metodo para cadastrar Imovel

--- src/main/java/br/com/brunolutterbach/stayhub/service/ImovelService.java
+++ src/main/java/br/com/brunolutterbach/stayhub/service/ImovelService.java
@@ -1,5 +1,8 @@
 package br.com.brunolutterbach.stayhub.service;
 
+import br.com.brunolutterbach.stayhub.model.imovel.DadosCadastroImovel;
+import br.com.brunolutterbach.stayhub.model.imovel.DadosListagemImovel;
+import br.com.brunolutterbach.stayhub.model.imovel.Imovel;
 import br.com.brunolutterbach.stayhub.repository.ImovelRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -10,4 +13,10 @@ public class ImovelService {
 
     final ImovelRepository repository;
 
+    public DadosListagemImovel cadastrarImovel(DadosCadastroImovel dados) {
+        var imovel = new Imovel(dados);
+        repository.save(imovel);
+        return new DadosListagemImovel(imovel);
+
+    }
 }
