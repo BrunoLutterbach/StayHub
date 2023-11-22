f713d208f926b7971285c64019280efe43a04e6c
Bruno <brunolutterbach13@gmail.com>
2023-11-22 20:41:31 -0300

    Metodos para atualizar, ativar, buscarporid, listar imoveis.

--- src/main/java/br/com/brunolutterbach/stayhub/service/ImovelService.java
+++ src/main/java/br/com/brunolutterbach/stayhub/service/ImovelService.java
@@ -1,10 +1,13 @@
 package br.com.brunolutterbach.stayhub.service;
 
+import br.com.brunolutterbach.stayhub.model.imovel.DadosAtualizacaoImovel;
 import br.com.brunolutterbach.stayhub.model.imovel.DadosCadastroImovel;
 import br.com.brunolutterbach.stayhub.model.imovel.DadosListagemImovel;
 import br.com.brunolutterbach.stayhub.model.imovel.Imovel;
 import br.com.brunolutterbach.stayhub.repository.ImovelRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -18,5 +21,28 @@ public class ImovelService {
         repository.save(imovel);
         return new DadosListagemImovel(imovel);
 
+    }
+
+    public DadosListagemImovel buscarPorId(Long id) {
+        var imovel = repository.getReferenceById(id);
+        return new DadosListagemImovel(imovel);
+    }
+
+    public Page<DadosListagemImovel> listarTodos(Pageable pageable) {
+        return repository.findAll(pageable).map(DadosListagemImovel::new);
+    }
+
+    public DadosListagemImovel ativarImovel(Long id) {
+        var imovel = repository.getReferenceById(id);
+        imovel.setAtivo(true);
+        repository.save(imovel);
+        return new DadosListagemImovel(imovel);
+    }
+
+    public DadosListagemImovel atualizarImovel(Long id, DadosAtualizacaoImovel dados) {
+        var imovel = repository.getReferenceById(id);
+        imovel.atualizar(dados);
+        repository.save(imovel);
+        return new DadosListagemImovel(imovel);
     }
 }
