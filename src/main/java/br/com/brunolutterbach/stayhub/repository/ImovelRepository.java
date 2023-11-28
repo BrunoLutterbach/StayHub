46947f14a031f224eee2a5f874f3f0b00f38dc29
Bruno <brunolutterbach13@gmail.com>
2023-11-28 19:36:20 -0300

    Adicinado método findByUf

--- src/main/java/br/com/brunolutterbach/stayhub/repository/ImovelRepository.java
+++ src/main/java/br/com/brunolutterbach/stayhub/repository/ImovelRepository.java
@@ -1,9 +1,16 @@
 package br.com.brunolutterbach.stayhub.repository;
 
+import br.com.brunolutterbach.stayhub.model.imovel.DadosListagemImovel;
 import br.com.brunolutterbach.stayhub.model.imovel.Imovel;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface ImovelRepository extends JpaRepository<Imovel, Long> {
+
+    Optional<DadosListagemImovel> findByUf(Pageable pageable, String uf);
 }
