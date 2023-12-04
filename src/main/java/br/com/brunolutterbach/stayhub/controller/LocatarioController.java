5950905a05e45b6c4372aceb5bb590999ff3510d
Bruno <brunolutterbach13@gmail.com>
2023-12-04 18:51:50 -0300

    Alterado Usuario para Locatario

--- src/main/java/br/com/brunolutterbach/stayhub/controller/LocatarioController.java
+++ src/main/java/br/com/brunolutterbach/stayhub/controller/LocatarioController.java
@@ -1,9 +1,9 @@
 package br.com.brunolutterbach.stayhub.controller;
 
-import br.com.brunolutterbach.stayhub.model.user.DadosAtualizacaoUsuario;
-import br.com.brunolutterbach.stayhub.model.user.DadosCadastroUsuario;
-import br.com.brunolutterbach.stayhub.model.user.DadosListagemUsuario;
-import br.com.brunolutterbach.stayhub.service.UsuarioService;
+import br.com.brunolutterbach.stayhub.model.locatario.DadosAtualizacaoLocatario;
+import br.com.brunolutterbach.stayhub.model.locatario.DadosCadastroLocatario;
+import br.com.brunolutterbach.stayhub.model.locatario.DadosListagemLocatario;
+import br.com.brunolutterbach.stayhub.service.LocatarioService;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -17,12 +17,12 @@ import javax.validation.Valid;
 @RestController
 @RequestMapping("/api/usuario")
 @AllArgsConstructor
-public class UsuarioController {
+public class LocatarioController {
 
-    final UsuarioService service;
+    final LocatarioService service;
 
     @PostMapping()
-    public ResponseEntity<DadosListagemUsuario> criarUsuario(@RequestBody @Valid DadosCadastroUsuario dados, UriComponentsBuilder uriBuilder) {
+    public ResponseEntity<DadosListagemLocatario> criarUsuario(@RequestBody @Valid DadosCadastroLocatario dados, UriComponentsBuilder uriBuilder) {
         var usuario = service.criarUsuario(dados);
         var uri = uriBuilder.path("/api/usuario/{id}").buildAndExpand(usuario.id()).toUri();
         return ResponseEntity.created(uri).body(usuario);
@@ -30,19 +30,19 @@ public class UsuarioController {
 
     @PutMapping("/{id}")
     @Transactional
-    public ResponseEntity<DadosListagemUsuario> atualizarUsuario(@PathVariable Long id, @RequestBody @Valid DadosAtualizacaoUsuario dados) {
+    public ResponseEntity<DadosListagemLocatario> atualizarUsuario(@PathVariable Long id, @RequestBody @Valid DadosAtualizacaoLocatario dados) {
         var usuario = service.atualizarUsuario(id, dados);
         return ResponseEntity.ok(usuario);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<DadosListagemUsuario> getUsuario(@PathVariable Long id) {
+    public ResponseEntity<DadosListagemLocatario> getUsuario(@PathVariable Long id) {
         var usuario = service.buscarPorId(id);
         return ResponseEntity.ok(usuario);
     }
 
     @GetMapping
-    public ResponseEntity<Page<DadosListagemUsuario>> listarTodos(Pageable pageable) {
+    public ResponseEntity<Page<DadosListagemLocatario>> listarTodos(Pageable pageable) {
         var usuario = service.listarTodos(pageable);
         return ResponseEntity.ok(usuario);
     }
