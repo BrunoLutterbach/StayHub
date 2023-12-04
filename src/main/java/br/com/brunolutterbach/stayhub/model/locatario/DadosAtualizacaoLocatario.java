2354f266bd9640e80b2a37850c0f75452740139a
Bruno <brunolutterbach13@gmail.com>
2023-12-04 18:46:00 -0300

    Renomeado Usuario para Locatario

---
+++ src/main/java/br/com/brunolutterbach/stayhub/model/locatario/DadosAtualizacaoLocatario.java
@@ -0,0 +1,9 @@
+package br.com.brunolutterbach.stayhub.model.locatario;
+
+
+public record DadosAtualizacaoLocatario(
+        String nome,
+        String senha,
+        String email
+) {
+}
--- src/main/java/br/com/brunolutterbach/stayhub/model/user/DadosAtualizacaoUsuario.java
+++
@@ -1,9 +0,0 @@
-package br.com.brunolutterbach.stayhub.model.user;
-
-
-public record DadosAtualizacaoUsuario(
-        String nome,
-        String senha,
-        String email
-) {
-}
